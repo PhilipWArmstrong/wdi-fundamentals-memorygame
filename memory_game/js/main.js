e42064a545974c82be9b98f01b55f7521f33bbeb
Philip Armstrong <philiparmstrong@Philips-MacBook-Pro.local>
2019-09-03 08:58:42 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and running!");
+var cards ["queen", "queen", "king", "king"];
+var cardsInPlay [];
 var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+var cardTwo = "king";
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+if (cardsInPlay = 2)
+cardsInPlay.length === 2;
+if (cardOne === cardTwo) {
+alert("You found a match!");
+} else if (cardOne > cardTwo) {
+alert("Sorry, try again.");
+}
